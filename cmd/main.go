8469e895b1b63681d530935cae73b1aae9d813d0
Muzzammil Shahid <muzzammil@xconn.io>
2025-02-10 15:27:43 +0500

    Run router in code

--- cmd/main.go
+++ cmd/main.go
@@ -6,12 +6,15 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"os/exec"
 	"strings"
 
 	"github.com/xconnio/xconn-go"
 )
+
+const TaskNotifierRealm = "io.xconn.task_notifier"
 
 func runCommand(name string, args ...string) (int, string) {
 	cmd := exec.Command(name, args...)
@@ -38,10 +41,35 @@ func runCommand(name string, args ...string) (int, string) {
 	return exitCode, strings.Join(outputLines, "\n")
 }
 
+func availablePort() (int, error) {
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return 0, err
+	}
+	defer func() { _ = l.Close() }()
+
+	return l.Addr().(*net.TCPAddr).Port, nil
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatalln("Must be called with at least one argument")
 	}
+
+	rout := xconn.NewRouter()
+	rout.AddRealm(TaskNotifierRealm)
+
+	port, err := availablePort()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	server := xconn.NewServer(rout, nil, nil)
+	closer, err := server.Start("0.0.0.0", port)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer closer.Close()
 
 	command := os.Args[1]
 	args := os.Args[2:]
@@ -49,12 +77,13 @@ func main() {
 
 	fmt.Printf("Exit Code: %d\nOutput:\n%s", exitCode, output)
 
-	session, err := xconn.Connect(context.Background(), "ws://localhost:8080/ws", "realm1")
+	session, err := xconn.Connect(context.Background(), fmt.Sprintf("ws://localhost:%v/ws", port), TaskNotifierRealm)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err := session.Publish("io.xconn.output", []any{exitCode, output}, nil, nil); err != nil {
+	if err := session.Publish("io.xconn.output", []any{exitCode, output}, nil,
+		map[string]any{"acknowledge": true}); err != nil {
 		log.Fatal(err)
 	}
 }
