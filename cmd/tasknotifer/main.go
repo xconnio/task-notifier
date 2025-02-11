30d236d62384e55ba9d945a2f60eb2e955526a8c
Muzzammil Shahid <muzzammil@xconn.io>
2025-02-11 16:20:16 +0500

    Add option to wait for a command

--- cmd/tasknotifer/main.go
+++ cmd/tasknotifer/main.go
@@ -2,48 +2,148 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"time"
 
+	"github.com/grandcat/zeroconf"
+
+	"github.com/xconnio/wampproto-go/util"
 	"github.com/xconnio/xconn-go"
 )
 
 const TaskNotifierRealm = "io.xconn.task_notifier"
 
 func main() {
-	if len(os.Args) < 2 {
+	flag.Parse()
+
+	waitTime, cliArgs := extractWaitFlag(flag.Args())
+	if len(cliArgs) < 1 {
 		log.Fatalln("Must be called with at least one argument")
 	}
 
+	if waitTime != 0 {
+		resolver, err := zeroconf.NewResolver(nil)
+		if err != nil {
+			log.Fatalln("Failed to initialize resolver:", err.Error())
+		}
+
+		entries := make(chan *zeroconf.ServiceEntry)
+		foundProcess := make(chan int)
+		go func(results <-chan *zeroconf.ServiceEntry) {
+			for entry := range results {
+				log.Println(entry.HostName, entry.Text, entry.Port)
+				if waitTime == entry.Port {
+					foundProcess <- entry.Port
+					return
+				}
+			}
+		}(entries)
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+		defer cancel()
+		err = resolver.Browse(ctx, "_tasknotifier._tcp", "local.", entries)
+		if err != nil {
+			log.Fatalln("Failed to browse:", err)
+		}
+
+		doneChan := make(chan struct{})
+		select {
+		case port := <-foundProcess:
+			session, err := xconn.Connect(context.Background(), fmt.Sprintf("ws://localhost:%v/ws", port),
+				TaskNotifierRealm)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			_, err = session.Subscribe("io.xconn.output", func(event *xconn.Event) {
+				exitCode, ok := util.AsInt64(event.Arguments[0])
+				if !ok {
+					log.Fatalln("Failed to parse exit code:", event.Arguments[0])
+					return
+				}
+				if exitCode != 0 {
+					log.Fatalf("Command was not successful: exit code: %v, logs: %s", exitCode,
+						event.Arguments[1])
+				}
+
+				if err := runCommandAndPublish(cliArgs); err != nil {
+					log.Fatalln(err)
+				}
+				doneChan <- struct{}{}
+			}, nil)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+		case <-ctx.Done():
+			log.Fatalln("Timeout: Could not find expected process")
+		}
+
+		<-doneChan
+	} else {
+		log.Fatalln(runCommandAndPublish(cliArgs))
+	}
+}
+
+func runCommandAndPublish(cliArgs []string) error {
 	rout := xconn.NewRouter()
 	rout.AddRealm(TaskNotifierRealm)
 
 	port, err := availablePort()
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
 	server := xconn.NewServer(rout, nil, nil)
 	closer, err := server.Start("0.0.0.0", port)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 	defer closer.Close()
 
-	command := os.Args[1]
-	args := os.Args[2:]
+	command := cliArgs[0]
+	args := cliArgs[1:]
+
+	hostname, _ := os.Hostname()
+	mdns, err := zeroconf.Register(hostname, "_tasknotifier._tcp", "local.", port,
+		[]string{fmt.Sprintf("%v", port)}, nil)
+	if err != nil {
+		return err
+	}
+	defer mdns.Shutdown()
+
 	exitCode, output := runCommand(command, args...)
 
 	fmt.Printf("Exit Code: %d\nOutput:\n%s", exitCode, output)
 
 	session, err := xconn.Connect(context.Background(), fmt.Sprintf("ws://localhost:%v/ws", port), TaskNotifierRealm)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	if err := session.Publish("io.xconn.output", []any{exitCode, output}, nil,
-		map[string]any{"acknowledge": true}); err != nil {
-		log.Fatal(err)
+	return session.Publish("io.xconn.output", []any{exitCode, output}, nil,
+		map[string]any{"acknowledge": true})
+}
+
+// extractWaitFlag extracts the value of --wait and removes it from the arguments.
+func extractWaitFlag(args []string) (int, []string) {
+	var filteredArgs []string
+	waitTime := 0
+
+	for i := 0; i < len(args); i++ {
+		if args[i] == "--wait" && i+1 < len(args) {
+			val, err := strconv.Atoi(args[i+1])
+			if err == nil {
+				waitTime = val
+			}
+			i++
+		} else {
+			filteredArgs = append(filteredArgs, args[i])
+		}
 	}
+	return waitTime, filteredArgs
 }
--- go.mod
+++ go.mod
@@ -2,23 +2,32 @@ module github.com/xconnio/task-notifier
 
 go 1.23.5
 
-require github.com/xconnio/xconn-go v0.0.0-20250111120502-52d5c439bf66
+require (
+	github.com/grandcat/zeroconf v1.0.0
+	github.com/xconnio/wampproto-go v0.0.0-20241111134208-d01416f5abc7
+	github.com/xconnio/xconn-go v0.0.0-20250111120502-52d5c439bf66
+)
 
 require (
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/fxamacker/cbor/v2 v2.6.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.4.0 // indirect
+	github.com/miekg/dns v1.1.56 // indirect
 	github.com/projectdiscovery/ratelimit v0.0.50 // indirect
 	github.com/projectdiscovery/utils v0.2.3 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
-	github.com/xconnio/wampproto-go v0.0.0-20241111134208-d01416f5abc7 // indirect
 	github.com/xconnio/wampproto-protobuf/go v0.0.0-20240706133816-0ca5f0268ce9 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/exp v0.0.0-20240525044651-4c93da0ed11d // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/tools v0.21.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 )
